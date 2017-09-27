903b96e58a09c49830cd98f5070aa3609161829c
EvelienHermans <emhermans98@gmail.com>
2017-09-27 12:38:02 +0200

    Appointment

--- app/src/main/java/Classes/Appointment.java
+++ app/src/main/java/Classes/Appointment.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class Appointment {
     private String Name;
     private Date reserveringsTijd;
+    private State state;
 
     private Room room;
 
@@ -18,6 +19,23 @@ public class Appointment {
         this.Name = Name;
         this.reserveringsTijd = reserveringsTijd;
         this.room = room;
+        this.state = State.Closed;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    public boolean open(){
+      if(reserveringsTijd.after(new Date()) && reserveringsTijd.before(new Date(System.currentTimeMillis()+5*60*1000))){
+          this.state = State.Open;
+          return true;
+      }
+      return false;
+    }
+
+    public void close(){
+        this.state = State.Closed;
     }
 
 
