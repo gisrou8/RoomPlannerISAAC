fa0be59b9cffb02f4d2f56ed4219bf7608e6d3f4
EvelienHermans <emhermans98@gmail.com>
2017-10-04 10:05:34 +0200

    Open/Close

--- app/src/main/java/com/example/gisro/roomplannerisaac/MainActivity.java
+++ app/src/main/java/com/example/gisro/roomplannerisaac/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.gisro.roomplannerisaac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Classes.Appointment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnOpenClose;
+
+    Appointment a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnOpenClose = (Button) findViewById(R.id.button);
+    }
+
+    public void btnVergaderingOnClick(View v){
+        if(btnOpenClose.getText() == "Open vergadering"){
+            a.open();
+            btnOpenClose.setText("Sluit vergadering");
+        }
+        else if(btnOpenClose.getText() == "Sluit vergadering"){
+            a.close();
+        }
     }
 }
